bd5a9dde5286cc49c035937c35de5171bce3fce1
Morax <James20081204@gmail.com>
2025-03-25 20:52:19 +0800

    Add astro-icon and update dependencies
    Introduces astro-icon to enhance icon support.
    Updates dependency resolutions and adds related type packages.
    Improves overall package lock integrity.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['"Times New Roman"', 'Times', 'serif'],
+      serif: ['"Times New Roman"', 'Times', 'serif'],
+    },
     extend: {
       typography: {
         DEFAULT: {
